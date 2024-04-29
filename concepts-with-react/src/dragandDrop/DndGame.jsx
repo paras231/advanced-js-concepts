a705d64958411016074e5af0a3196b9979fb1546
paras231 <sharmaparas553@gmail.com>
2024-04-29 21:54:32 +0530

    updated code for dnd game

--- concepts-with-react/src/dragandDrop/DndGame.jsx
+++ concepts-with-react/src/dragandDrop/DndGame.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import gameover_sound from "../assets/gameover.wav";
 const DndGame = () => {
   const [boxes, setBoxes] = useState([
@@ -17,6 +17,10 @@ const DndGame = () => {
   const [coundDownTimer, setCoundDownTimer] = useState(30);
   const [gameOver, setGameOver] = useState(false);
   const [gameOverAudio, setGameOverAudio] = useState(false);
+  const audioRef = useRef(null);
+
+  // store refs in object as we have list of divs
+  const draggableRefs = useRef({});
   useEffect(() => {
     const timerVal = setTimeout(() => setTimer((t) => t - 1), 1000);
     if (timer == 0) {
@@ -53,8 +57,98 @@ const DndGame = () => {
   }
 
   //  play game over sound
+  //  modern web browsers do not allow auto play audio
+  //  the audio is only played when there is some user interaction like a button click.
+  //  so I will have to look for different solution
+  function playAudio() {
+    const promise = audioRef.current?.play();
+    console.log(promise);
+    if (promise !== "undefined") {
+      promise
+        .then(() => {})
+        .catch((err) => {
+          audioRef.current && (audioRef.current.muted = true);
+          audioRef.current && audioRef.current.play();
+        });
+    }
+  }
 
-  
+  // useEffect(() => {
+  //   if (gameOver) {
+  //     playAudio();
+  //   }
+  // }, [gameOver]);
+
+  // handle drag and drop logic for game
+
+  function onDragStart(e, id) {
+    const dataIndex = e.target.getAttribute("data-index");
+
+    if (id == dataIndex) {
+      const draggableRef = draggableRefs[id];
+
+      // we do not get draggablref.current as it is undefined
+      //  so we cam directly update the style of the element
+      if (draggableRef.style) {
+        draggableRef.style.opacity = "0.6";
+      }
+    }
+  }
+
+  //  handle on drag enter
+
+  function onDragEnter(e, id) {
+    const dataIndex = e.target.getAttribute("data-index");
+
+    if (id == dataIndex) {
+      const draggableRef = draggableRefs[id];
+
+      // we do not get draggablref.current as it is undefined
+      //  so we cam directly update the style of the element
+      if (draggableRef.style) {
+        draggableRef.style.background = "pink";
+        draggableRef.style.cursor = "";
+      }
+    }
+  }
+
+  // handle drag leave
+
+  function onDragLeave(e, id) {
+    const dataIndex = e.target.getAttribute("data-index");
+
+    if (id == dataIndex) {
+      const draggableRef = draggableRefs[id];
+
+      // we do not get draggablref.current as it is undefined
+      //  so we cam directly update the style of the element
+      if (draggableRef.style) {
+        draggableRef.style.background = "";
+        draggableRef.style.opacity = "1";
+      }
+    }
+  }
+
+  // handle on drag over
+
+  function onDragOver(e,obj) {
+    e.stopPropagation();
+    e.preventDefault();
+    const text = e.target.textContent;
+    console.log(text);
+    e.dataTransfer.dropEffect = "copy";
+    const items = e.dataTransfer.items;
+    items.add(text, "text/plain");
+  }
+
+  // handle drop event
+
+  function onDrop(e) {
+    e.preventDefault();
+    //  console.log(e);
+    // on dropping the element we would need to shuffle the elements order
+    //  we would need to update the array state
+  }
 
   return (
     <>
@@ -62,7 +156,7 @@ const DndGame = () => {
         style={{
           background: "linear-gradient(270deg, #078cdf 0%, #9f7ff5 100%)",
         }}
-        className="container overflow-hidden p-4 text-white h-[100vh]"
+        className="container overflow-hidden p-4 text-white"
       >
         {gameStarted === false && <TimerComponent time={timer} />}
         {gameStarted && !gameOver && (
@@ -70,7 +164,7 @@ const DndGame = () => {
         )}
 
         {gameOver && (
-          <div className="flex mt-10 justify-center items-center flex-col">
+          <div className="flex h-[100vh] mt-10 justify-center items-center flex-col">
             <h1 className="text-[10rem]">Game Over!</h1>
             <button
               onClick={restartGame}
@@ -82,14 +176,21 @@ const DndGame = () => {
         )}
 
         {!gameOver && gameStarted && (
-          <section className="flex justify-center items-center flex-col space-y-4">
+          <section className="flex  justify-center items-center flex-col space-y-4">
             {boxes.map((b) => {
               return (
                 <div
+                  onDragStart={(e) => onDragStart(e, b.id)}
+                  onDragEnter={(e) => onDragEnter(e, b.id)}
+                  onDragOver={(e) => onDragOver(e, b)}
+                  onDrop={onDrop}
+                  onDragLeave={(e) => onDragLeave(e, b.id)}
                   draggable
                   style={{ border: "1px solid white" }}
-                  className="flex w-[20vw] cursor-pointer rounded-md p-4 flex-col"
+                  className="flex w-[20vw] cursor-move rounded-md p-4 flex-col"
                   key={b.id}
+                  ref={(ref) => (draggableRefs[b.id] = ref)}
+                  data-index={b.id}
                 >
                   <span className="text-xl">{b.title}</span>
 
@@ -99,8 +200,8 @@ const DndGame = () => {
             })}
           </section>
         )}
-        {}
-        <audio src={gameover_sound}></audio>
+
+        <audio ref={audioRef} autoPlay={gameOver} src={gameover_sound}></audio>
       </main>
     </>
   );
@@ -111,7 +212,7 @@ export default DndGame;
 const TimerComponent = ({ time }) => {
   return (
     <>
-      <div className="flex mt-20 justify-center items-center">
+      <div className="flex h-[100vh] mt-20 justify-center items-center">
         <h1 className="text-[10rem]">{time}</h1>
       </div>
     </>
